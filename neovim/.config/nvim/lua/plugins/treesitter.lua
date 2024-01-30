d49231aef3a8dbc7510513a4e1844d182761315d
simkestuff <simkestuff@gmail.com>
2024-01-30 19:48:13 +0100

    disable tab wiht autocompletion

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,29 +1,34 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-    },
-    build = function()
-        require("nvim-treesitter.install").update({ with_sync = true })
-    end,
-    event = {"BufReadPre", "BufNewFile"},
-    config = function()
-        local configs = require("nvim-treesitter.configs")
+  "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
+  build = function()
+    require("nvim-treesitter.install").update({ with_sync = true })
+  end,
+  event = {"BufReadPre", "BufNewFile"},
+  config = function()
+    local configs = require("nvim-treesitter.configs")
 
-        configs.setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "haskell" },
-            sync_install = false,
-            highlight = { enable = true },
-            indent = { enable = true },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = '<c-space>',
-                    node_incremental = '<c-space>',
-                    scope_incremental = '<c-s>',
-                    node_decremental = 'bs',
-                },
-            },
-        })
-    end,
+    configs.setup({
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "haskell" },
+      sync_install = false,
+      auto_install = true,
+      ignore_install = {},
+      highlight = { enable = true },
+      indent = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<c-space>',
+          node_incremental = '<c-space>',
+          scope_incremental = '<c-s>',
+          node_decremental = 'bs',
+        },
+      },
+      textobjects = {
+        enable = true,
+      },
+    })
+  end,
 }
