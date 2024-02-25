3acad11d477d275568e39f2e9cae8cdf8b7fbfce
simkestuff <simkestuff@gmail.com>
2024-02-25 10:55:51 +0100

    remove format on save, add fidget

--- neovim/.config/nvim/lua/lsp.lua
+++ neovim/.config/nvim/lua/lsp.lua
@@ -1,6 +1,6 @@
   -- [[ Configure LSP ]]
   --  This function gets run when an LSP connects to a particular buffer.
-  local on_attach = function(_, bufnr)
+  local on_attach = function(client, bufnr)
 
     vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, { buffer = bufnr, desc = "Rename"})
     vim.keymap.set("n", "<leader>lc", vim.lsp.buf.code_action, { buffer = bufnr, desc = "Code Action"})
@@ -17,6 +17,15 @@
     vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
       vim.lsp.buf.format()
     end, { desc = 'Format current buffer with LSP' })
+
+    -- format on save
+    -- if client.server_capabilities.documentFormattingProvider then
+    --   vim.api.nvim_create_autocmd("BufWritePre", {
+    --     group = vim.api.nvim_create_augroup("Format", { clear = true }),
+    --     buffer = bufnr,
+    --     callback = function() vim.lsp.buf.formatting_seq_sync() end
+    --   })
+    -- end
   end
 
   -- decorate floating windows
--- neovim/.config/nvim/lua/plugins/lsp-config.lua
+++ neovim/.config/nvim/lua/plugins/lsp-config.lua
@@ -8,7 +8,7 @@ return {
 
     -- Useful status updates for LSP
     -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-    -- { 'j-hui/fidget.nvim', opts = {}, tag = "legacy", pin = true },
+    { 'j-hui/fidget.nvim', opts = {}, tag = "v1.0.0", pin = true },
 
     -- Additional lua configuration, makes nvim stuff amazing!
     'folke/neodev.nvim',
