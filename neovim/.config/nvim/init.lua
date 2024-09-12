6168a6d7debab62bd4bfb0e0d03acedcc9905282
simkestuff <simkestuff@gmail.com>
2024-09-12 15:01:15 +0200

    dorade zbog vscode

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,23 +1,22 @@
-require("sinisa.remap")
-require("sinisa.options")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
 
 if vim.g.vscode then
-    vim.cmd[[source $HOME/.config/nvim/vscode/settings.vim]]
+	vim.cmd([[source $HOME/.config/nvim/vscode/settings.lua]])
+	require("lazy").setup("vsc", {})
 else
-    local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-    if not vim.loop.fs_stat(lazypath) then
-      vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-      })
-    end
-    vim.opt.rtp:prepend(lazypath)
+	require("sinisa.remap")
+	require("sinisa.options")
 
-    require("lazy").setup("plugins", {})
-    -- require("lsp")
-
+	require("lazy").setup("plugins", {})
 end
