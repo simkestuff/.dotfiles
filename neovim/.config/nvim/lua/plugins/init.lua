035bcc1d4ca4b40d4625e6cb11d44f415e8f80e5
simkestuff <simkestuff@gmail.com>
2024-09-30 22:35:55 +0200

    add which-key

--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -100,4 +100,22 @@ return {
 			vim.g.vimtex_view_general_viewer = "okular"
 		end,
 	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	},
 }
