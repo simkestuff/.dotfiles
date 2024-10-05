435932810fe5f59c60b24d96d3832b232dd172c1
simkestuff <simkestuff@gmail.com>
2024-10-05 20:45:05 +0200

    wezterm rekonfiguracija
    preradio sam wezterm konfiguraciju kako bi sličila tmux priči.

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
-
-local appearance = require("appearance")
+local act = wezterm.action
+local mux = wezterm.mux
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -12,58 +12,117 @@ config.font = wezterm.font_with_fallback({ family = "JetBrains Mono", weight = "
 
 -- color scheme:
 --
-config.color_scheme = "Kanagawa Wave"
+config.color_scheme = "Google (light) (terminal.sexy)"
 
 -- disable audio bell
 --
 config.audible_bell = "Disabled"
 
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 }
 config.keys = {
 	{
-		key = "t",
-		mods = "ALT",
+		-- šalje ctrl-a u terminalu
+		key = "a",
+		mods = "LEADER|CTRL",
+		action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }),
+	},
+	-- kreira novi prozor
+	{
+		key = "c",
+		mods = "LEADER",
 		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
 	},
+	-- toggle max panel
 	{
-		key = "s",
-		mods = "CTRL|ALT",
+		key = "z",
+		mods = "LEADER",
+		action = wezterm.action.TogglePaneZoomState,
+	},
+	-- vertikalni split
+	{
+		key = '"',
+		mods = "LEADER|SHIFT",
 		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
 	},
+	-- horizontalni split
 	{
-		key = "v",
-		mods = "CTRL|ALT",
+		key = "-",
+		mods = "LEADER",
 		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
 	},
 	{
 		key = "h",
-		mods = "CTRL|ALT",
+		mods = "LEADER",
 		action = wezterm.action.ActivatePaneDirection("Left"),
 	},
 	{
 		key = "l",
-		mods = "CTRL|ALT",
+		mods = "LEADER",
 		action = wezterm.action.ActivatePaneDirection("Right"),
 	},
 	{
 		key = "j",
-		mods = "CTRL|ALT",
+		mods = "LEADER",
 		action = wezterm.action.ActivatePaneDirection("Down"),
 	},
 	{
 		key = "k",
-		mods = "CTRL|ALT",
+		mods = "LEADER",
 		action = wezterm.action.ActivatePaneDirection("Up"),
 	},
+	-- rename tab (window)
+	{
+		key = ",",
+		mods = "LEADER",
+		action = act.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, pane, line)
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
+	-- biraj koji prozor želiš
+	{
+		key = "w",
+		mods = "LEADER",
+		action = act.ShowTabNavigator,
+	},
+	-- zatvori trenutni prozor(tab)
+	{
+		key = "&",
+		mods = "LEADER|SHIFT",
+		action = act.CloseCurrentTab({ confirm = true }),
+	},
+	-- preimenuj sesiju
+	{
+		key = "$",
+		mods = "LEADER|SHIFT",
+		action = act.PromptInputLine({
+			description = "Enter new name for session",
+			action = wezterm.action_callback(function(window, pane, line)
+				if line then
+					mux.rename_workspace(window:mux_window():get_workspace(), line)
+				end
+			end),
+		}),
+	},
+	-- kreiraj novu sesiju ili se prebaci ne neku drugu sesiju
+	{
+		key = "s",
+		mods = "LEADER",
+		action = act.ShowLauncherArgs({ flags = "WORKSPACES" }),
+	},
 }
+-- ctrl-num prebacuje na num prozor
 for i = 1, 8 do
 	table.insert(config.keys, {
 		key = tostring(i),
-		mods = "ALT",
+		mods = "CTRL",
 		action = wezterm.action.ActivateTab(i - 1),
 	})
 end
-
-config.use_fancy_tab_bar = false
 
 config.enable_scroll_bar = false
 config.window_padding = {
@@ -73,6 +132,8 @@ config.window_padding = {
 	bottom = 0,
 }
 
+config.use_fancy_tab_bar = true
 config.tab_bar_at_bottom = true
+config.tab_max_width = 32
 
 return config
