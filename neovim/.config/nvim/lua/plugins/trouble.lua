196e2129855727c42a318ac78b0a80a2c9ce0ce4
simkestuff <simkestuff@gmail.com>
2024-11-05 21:19:32 +0100

    add trouble and treesitter objects to neovim

---
+++ neovim/.config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,42 @@
+return {
+	"folke/trouble.nvim",
+	opts = {}, -- for default options, refer to the configuration section for custom setup.
+	cmd = "Trouble",
+	keys = {
+		{
+			"<leader>xx",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+		{
+			"<leader>xd",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Buffer Diagnostics (Trouble)",
+		},
+		{
+			"<leader>xs",
+			"<cmd>Trouble symbols toggle focus=false pinned=true win.relative=win win.position=right<cr>",
+			desc = "Symbols (Trouble)",
+		},
+		{
+			"<leader>xr",
+			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+			desc = "LSP Definitions / references / ... (Trouble)",
+		},
+		{
+			"<leader>xl",
+			"<cmd>Trouble loclist toggle<cr>",
+			desc = "Location List (Trouble)",
+		},
+		{
+			"<leader>xq",
+			"<cmd>Trouble qflist toggle<cr>",
+			desc = "Quickfix List (Trouble)",
+		},
+		{
+			"<leader>xt",
+			"<cmd>Trouble todo toggle<cr>",
+			desc = "TODO List (Trouble)",
+		},
+	},
+}
