eceb4c49e7cb0aef49f35498d8c85ae93b406df1
simkestuff <simkestuff@gmail.com>
2024-11-07 21:58:38 +0100

    change neovim git configuration

--- neovim/.config/nvim/lua/plugins/git.lua
+++ neovim/.config/nvim/lua/plugins/git.lua
@@ -1,28 +1,28 @@
 return {
-	{
-		"NeogitOrg/neogit",
-		dependencies = {
-			"nvim-lua/plenary.nvim", -- required
-			"sindrets/diffview.nvim", -- optional - Diff integration
-
-			-- Only one of these is needed, not both.
-			"nvim-telescope/telescope.nvim", -- optional
-			-- "ibhagwan/fzf-lua", -- optional
-		},
-		config = function()
-			local neogit = require("neogit")
-			neogit.setup({
-				integrations = {
-					telescope = true,
-					diffview = true,
-				},
-				vim.keymap.set("n", "<leader>g", "<cmd>Neogit<CR>", { desc = "Git status" }),
-			})
-		end,
-	},
-	{
-		"tpope/vim-fugitive",
-	},
+	-- {
+	-- 	"NeogitOrg/neogit",
+	-- 	dependencies = {
+	-- 		"nvim-lua/plenary.nvim", -- required
+	-- 		"sindrets/diffview.nvim", -- optional - Diff integration
+	--
+	-- 		-- Only one of these is needed, not both.
+	-- 		"nvim-telescope/telescope.nvim", -- optional
+	-- 		-- "ibhagwan/fzf-lua", -- optional
+	-- 	},
+	-- 	config = function()
+	-- 		local neogit = require("neogit")
+	-- 		neogit.setup({
+	-- 			integrations = {
+	-- 				telescope = true,
+	-- 				diffview = true,
+	-- 			},
+	-- 			vim.keymap.set("n", "<leader>g", "<cmd>Neogit<CR>", { desc = "Git status" }),
+	-- 		})
+	-- 	end,
+	-- },
+	-- {
+	-- 	"tpope/vim-fugitive",
+	-- },
 	{
 		-- Adds git releated signs to the gutter, as well as utilities for managing changes
 		"lewis6991/gitsigns.nvim",
@@ -100,6 +100,28 @@ return {
 					{ buffer = bufnr, desc = "Preview hunk" }
 				)
 			end,
+		},
+	},
+	{
+		{
+			"kdheepak/lazygit.nvim",
+			lazy = true,
+			cmd = {
+				"LazyGit",
+				"LazyGitConfig",
+				"LazyGitCurrentFile",
+				"LazyGitFilter",
+				"LazyGitFilterCurrentFile",
+			},
+			-- optional for floating window border decoration
+			dependencies = {
+				"nvim-lua/plenary.nvim",
+			},
+			-- setting the keybinding for LazyGit with 'keys' is recommended in
+			-- order to load the plugin when the command is run for the first time
+			keys = {
+				{ "<leader>g", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+			},
 		},
 	},
 }
